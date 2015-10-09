403e5f9579463e0db2b3b8fc06938c37e517394a
neverkas <emmanuel.lazarte.011@gmail.com>
2015-10-09 19:28:50 -0300

    Se agregó api

---
+++ README.md
@@ -0,0 +1,3 @@
+# api-test
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/ProyectosController.js
@@ -0,0 +1,11 @@
+/**
+ * ProyectosController
+ *
+ * @description :: Server-side logic for managing proyectos
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
